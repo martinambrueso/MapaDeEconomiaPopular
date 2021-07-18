b17df3c1acac9d5912281988e625cc2178b6f56e
ma_e90832 <m.ambrueso@redlink.com.ar>
2021-07-18 14:45:07 -0300

    Solo se toca swagger

--- app/controllers/businessAreas.js
+++ app/controllers/businessAreas.js
@@ -1,17 +1,6 @@
 const businessAreaService = require('../services/businessAreas');
 
 exports.getAll = (req, res, next) => {
-  /* #swagger.tags = ['Main']
-        #swagger.description = 'Endpoint para adicionar um usuário.' */
-
-  /* #swagger.parameters['Bearer'] = {
-          name: 'authorization',
-          in: 'header',
-          description: 'Bearer + token.',
-          required: true,
-          type: 'string',
-          schema: { $ref: "#/definitions/UserSignUp" }
-  } */
   businessAreaService
     .getAll()
     .then(businessArea => res.send(businessArea))
--- app/controllers/entities.js
+++ app/controllers/entities.js
@@ -1,18 +1,6 @@
 const entitiesService = require('../services/entities');
 
 exports.createOne = (req, res, next) => {
-  /* #swagger.tags = ['Main']
-        #swagger.description = 'Endpoint para adicionar um usuário.' */
-
-  /* #swagger.parameters['Bearer'] = {
-          name: 'authorization',
-          in: 'header',
-          description: 'Bearer + token.',
-          required: true,
-          type: 'string',
-          schema: { $ref: "#/definitions/UserSignUp" }
-  } */
-  
   if(req.body){
     entitiesService
     .createOne(req.body)
